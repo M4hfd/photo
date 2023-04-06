b441b53b1154f1b05978fbdd057f2f63e4c6d26c
M4hfd <83091151+M4hfd@users.noreply.github.com>
2023-04-06 16:17:04 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+from PIL import Image
+import os
+
+for photo in range(5):
+    image = Image.open(f"photo_{photo}.jpg")
+    instagram = image.resize((1080, 1080))
+    coordinates = (10, 0, instagram.width-10, instagram.height)
+    vk = image.resize((1400, 1000))
+    instagram = instagram.crop(coordinates)
+    facebook = image.resize((1200, 628))
+    os.mkdir(f"photo_{photo}_end")
+    vk.save(f"photo_{photo}_end/photo_vk_{photo}.png", "png")
+    instagram.save(f"photo_{photo}_end/photo_instagram_{photo}.png", "png")
+    facebook.save(f"photo_{photo}_end/photo_facebook_{photo}.png", "png")
+
